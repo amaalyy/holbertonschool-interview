f5e57d38d4dabc134c358b788a4adc3d69762d6a
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-05-08 22:24:36 +0100

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
+"""
+Lockboxes
+"""
+
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-    
-    keys = [0]
-    visited = [False] * len(boxes)
-    visited[0] = True
-    
-    while keys:
-        current_key = keys.pop()
-        current_box = boxes[current_key]
-        
-        for key in current_box:
-            if 0 <= key < len(boxes) and not visited[key]:
-                visited[key] = True
-                keys.append(key)
-    
-    return all(visited)
+    """
+    method that determines if all the boxes can be opened.
+    """
+    n = len(boxes)
+    opened = [False] * n
+    Result = [0]
 
+    opened[0] = True
 
-test_cases = [
-    [[1], [2], [3], [4], []],
-    [[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]],
-    [[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]],
-    [[0]],
-]
+    while Result:
+        current_box = Result.pop()
 
-for i, boxes in enumerate(test_cases):
-    result = canUnlockAll(boxes)
-    print(f"Correct output - case {i + 1}: {result}")
+        for key in boxes[current_box]:
+            if 0 <= key < n and not opened[key]:
+                opened[key] = True
+                Result.append(key)
+
+    return all(opened)

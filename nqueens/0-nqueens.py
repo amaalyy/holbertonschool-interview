805c8695d85a3a78dd92e9afa2e5041e32f7fc76
amaalyy <135268152+amaalyy@users.noreply.github.com>
2024-08-01 21:42:25 +0100

    N queens

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -1,73 +1,73 @@
 #!/usr/bin/python3
+"""
+The N queens puzzle
+"""
+
+
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+try:
+    int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+if not isinstance(int(sys.argv[1]), int):
+    print("N must be a number")
+    exit(1)
 
-def is_number(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-
-def print_solution(board):
-    solution = []
-    for i in range(len(board)):
-        for j in range(len(board)):
-            if board[i][j] == 1:
-                solution.append([i, j])
-    print(solution)
-
-
-def is_safe(board, row, col):
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
+n = int(sys.argv[1])
 
 
-def solve_nqueens_util(board, col):
-    if col >= len(board):
-        print_solution(board)
-        return True
+def is_valid_state(state, n):
+    return len(state) == n
 
-    res = False
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            res = solve_nqueens_util(board, col + 1) or res
-            board[i][col] = 0
 
+def get_candidates(state, n):
+    if not state:
+        return range(n)
+
+    position = len(state)
+    candidates = set(range(n))
+    for row, col in enumerate(state):
+        candidates.discard(col)
+        dist = position - row
+        candidates.discard(col + dist)
+        candidates.discard(col - dist)
+    return candidates
+
+
+def search(state, solutions, n):
+    if is_valid_state(state, n):
+        state_string = state_to_string(state)
+        solutions.append(state_string)
+        return
+
+    for candidate in get_candidates(state, n):
+        state.append(candidate)
+        search(state, solutions, n)
+        state.pop()
+
+
+def solveNQueens(n):
+    solutions = []
+    state = []
+    search(state, solutions, n)
+    return solutions
+
+
+def state_to_string(state):
+    res = []
+
+    for x, y in enumerate(state):
+        res.append([x, y])
     return res
 
 
-def solve_nqueens(N):
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    if not solve_nqueens_util(board, 0):
-        print("No solution exists")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    N = sys.argv[1]
-
-    if not is_number(N):
-        print("N must be a number")
-        sys.exit(1)
-
-    N = int(N)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    solve_nqueens(N)
+for solution in solveNQueens(n):
+    print(solution)
